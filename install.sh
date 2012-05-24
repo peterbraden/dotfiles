b1d9cc13acada144da75016bbb1dcbfde43ae13f
Peter Braden <peterbraden@peterbraden.co.uk>
2012-05-24 10:53:58 -0700

    gitignore

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 DOTPATH=`pwd`
 ln -s $DOTPATH/gitconfig ~/.gitconfig
+ln -s $DOTPATH/gitignore ~/.gitignore
 ln -s $DOTPATH/hgrc ~/.hgrc
 ln -s $DOTPATH/profile ~/.profile
 ln -s $DOTPATH/screenrc ~/.screenrc
 ln -s $DOTPATH/vimrc ~/.vimrc
 ln -s $DOTPATH/zshrc ~/.zshrc
 ln -s $DOTPATH/vim ~/.vim
+
 
 chsh -s /bin/zsh
