ec6abf6314975494e51ba21587d8f2591331453b
Peter Braden <peterbraden@peterbraden.co.uk>
2016-04-14 16:29:07 +0200

    installl vim

--- linux/apt.sh
+++ linux/apt.sh
@@ -7,4 +7,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 sudo apt-get update
 sudo apt-get upgrade
 
-apt-get install build-essential
+sudo apt-get install -y build-essential
+
+
+sudo apt-get install -y vim
