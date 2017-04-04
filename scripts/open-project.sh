3a48446bae48c22d842f51fe08f9610e5fc477ee
Peter Braden <peterbraden@peterbraden.co.uk>
2017-04-04 09:10:59 +0000

    handle repos with dots in the name

--- scripts/open-project.sh
+++ scripts/open-project.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 SESSION=$1
 DIRECTORY=~/repos/$SESSION
+SESSION_NO_DOTS=${SESSION/\./-}
 
-if tmux info &> /dev/null && tmux has-session -t $SESSION; then
-  echo "reattaching to $SESSION"
-  tmux attach -t $SESSION
+if tmux info &> /dev/null && tmux has-session -t $SESSION_NO_DOTS; then
+  echo "reattaching to $SESSION_NO_DOTS"
+  tmux attach -t $SESSION_NO_DOTS
   exit 2
 fi
 
-echo "starting new session $SESSION $DIRECTORY"
-tmux new-session -s $SESSION -c $DIRECTORY -d
+echo "starting new session $SESSION_NO_DOTS $DIRECTORY"
+tmux new-session -s $SESSION_NO_DOTS -c $DIRECTORY -d
 
-tmux new-window -t $SESSION:1 -c $DIRECTORY
-tmux new-window -t $SESSION:2 -c $DIRECTORY
-tmux select-window -t $SESSION:0
+tmux new-window -t $SESSION_NO_DOTS:1 -c $DIRECTORY
+tmux new-window -t $SESSION_NO_DOTS:2 -c $DIRECTORY
+tmux select-window -t $SESSION_NO_DOTS:0
 tmux send-keys "~/repos/dotfiles/motd" C-m
 
-tmux attach-session -t $SESSION
+tmux attach-session -t $SESSION_NO_DOTS
 
 
