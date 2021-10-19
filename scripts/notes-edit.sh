bd303f7d74b45fa10694a014b8d912698229643d
Peter Braden <peterbraden@peterbraden.co.uk>
2021-10-19 10:06:58 +0000

    shell scripts

---
+++ scripts/notes-edit.sh
@@ -0,0 +1,29 @@
+if [ -z "$NOTES_DIR" ]; then 
+  NOTES_DIR=~/Dropbox/Notebooks/
+fi
+
+
+# $@ join to see name
+find_most_likely_note() {
+  QUERY="$@"
+  # TODO use agrep or similar to do fuzzy matching
+  FILE=$(find $NOTES_DIR -type f -printf '%T@ %p\n' | grep -i "$QUERY" | sort -r | head -1 | cut -d " " -f 2- )
+
+  if [ -z "$FILE" ]; then 
+    echo "No notes found for ${QUERY}"
+    exit 1
+  fi
+  echo $FILE
+}
+
+
+edit_most_likely() {
+  if [ -z "$@" ]; then 
+    echo "Needs a query."
+    exit 1
+  fi
+  vi "$(find_most_likely_note $@)"
+}
+
+edit_most_likely $@
+
---
+++ scripts/rclone.sh
@@ -0,0 +1,33 @@
+# Tested with rclone v1.56.2
+
+# Creates remote 'aws' for s3
+create_s3_remote(){
+  CONFIG=$(rclone config file | sed 's/Configuration file is stored at://g' | tr '\n' ' ' | sed 's/ //g')
+  MATCH=$(grep '\[aws\]' ${CONFIG})
+  if [ -z "$MATCH" ]; then
+    echo "Remote 'aws' not found - creating..."
+    echo "\n[aws]\ntype = s3\nprovider = AWS\nenv_auth = true\nregion = us-east-1\nacl = private" >> ${CONFIG}
+  else
+    echo "Remote already exists"
+  fi
+}
+
+#$1 = name
+create_bucket(){
+  NAME=$1
+  rclone mkdir aws:$NAME
+}
+
+create_encrypted_s3_bucket(){
+  #NB This doesn't work yet, run config manually through the menus to get the 
+  #   Passphrase generation etc.
+  exit 1
+  NAME=$1
+  create_s3_remote
+  create_bucket $NAME
+  echo "Creating crypt $NAME"
+  rclone config create "$NAME" crypt --crypt-remote=aws:$NAME
+
+}
+
+
