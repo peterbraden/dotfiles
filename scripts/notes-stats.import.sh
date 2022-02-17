2b95c087ef4f9d6441fb70dff541d2c656c70a67
Peter Braden <peterbraden@peterbraden.co.uk>
2022-02-17 10:21:08 +0000

    wip on stats

--- scripts/notes-stats.import.sh
+++ scripts/notes-stats.import.sh
@@ -6,13 +6,22 @@ notes_stats () {
   cd $NOTES_DIR
 
   total=0
-  for sha in $(git rev-list --since="6am" --abbrev-commit master | sed -e '$ d'); do
+  files_changed=""
+  since=$(date --date="2 days ago" --rfc-3339=date)
+  first_sha=$(git log --format=format:%H --since="$since" | tail -1)
+
+
+  for sha in $(git rev-list --since="$since" --abbrev-commit master | sed -e '$ d'); do
     added=$(git diff --word-diff=porcelain $sha~1..$sha|grep -e"^+[^+]"|wc -w|xargs)
     deleted=$(git diff --word-diff=porcelain $sha~1..$sha|grep -e"^-[^-]"|wc -w|xargs)
     duplicated=$(git diff $sha~1..$sha|grep -e"^+[^+]" -e"^-[^-]"|sed -e's/.//'|sort|uniq -d|wc -w|xargs)
     date=$(git show --no-patch --no-notes --pretty='%ci' $sha | cut -c1-16)
     prefix="- $sha $date :"
     total=$((total+added))
+
+    changed=$(git diff --name-only $sha; echo x) # Bash sucks - trims trailing newlines
+    changed="${changed%?}" #strip the trailing x
+    files_changed+="$changed"
     if [ "$added" -eq "0" ]; then
       changed=$deleted
       echo "$prefix added:" $added, "deleted:" $deleted, "duplicated:"\
@@ -26,6 +35,9 @@ notes_stats () {
                        $duplicated, "changes counted:" $changed
     fi
     done
+
+    echo "# Changes since $since"
+    echo "$files_changed" | sort | uniq
     echo "Total words added:", $total
-    git log --shortstat --since="1 day ago" | grep "files changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print "Files changed:", files, ", lines inserted:", inserted, ", lines deleted:", deleted}'
+    git log --shortstat --since="$since" | grep "files changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print "Files changed:", files, ", lines inserted:", inserted, ", lines deleted:", deleted}'
 }
--- scripts/notes-stats.sh
+++ scripts/notes-stats.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-source './notes-stats.import.sh'
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source "$SCRIPT_DIR/notes-stats.import.sh"
 
 notes_stats
