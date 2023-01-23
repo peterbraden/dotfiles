e6c3d1f82bf8a939bbd187da5f2eb2379e31498b
Peter Braden <peterbraden@peterbraden.co.uk>
2023-01-23 18:46:05 +0100

    remove makefile

--- run_install.sh
+++ run_install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "Running peterbraden/dotfiles install.sh"
+echo "# Running peterbraden/dotfiles install.sh"
 
 if [ "$SHELL" != "/bin/zsh" ]; then 
   echo "# - Changing shell to zsh\n"
   sudo chsh -s $(which zsh) $USER;
 fi
 
+DOTPATH=`pwd`
+
 if [ $(uname -s) == "Darwin" ]; then
-	echo "- Setting up a mac"
+	echo "# - Setting up a mac"
 	#$(DOTPATH)/osx/install.sh
 	#$(DOTPATH)/osx/osx.sh
 	#$(DOTPATH)/osx/apps.sh
