92a5957a50606430701e87de4d24acc745a9f2d4
Peter Braden <peterbraden@peterbraden.co.uk>
2023-07-27 13:59:04 +0000

    install pass

--- home/run_once_packages.sh
+++ home/run_once_packages.sh
@@ -51,6 +51,7 @@ if [ "$(uname -s)" == "Linux" ]; then
   sudo apt-get install -y awscli zsh vim
   sudo apt-get install -y mosh
   sudo apt-get install -y neovim
+  sudo apt-get install -y pass
   # TODO tailscale
   #
   #apt install bat
