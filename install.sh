fe995fd4c308945967c5bd23c32eb1d070c906d7
Peter Braden <peterbraden@peterbraden.co.uk>
2024-10-04 13:54:42 +0200

    codespaces is dumb

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@
 # Github codespaces {{{
 if [ -n "$CODESPACES" ]; then
   echo "Setting up a dev environment inside github codespaces..."
+
+  # Github auth in codespaces defaults to http
+  # Ref: https://docs.github.com/en/codespaces/troubleshooting/troubleshooting-authentication-to-a-repository#authenticating-to-the-repository-you-created-the-codespace-from
+  git config --global url."git@github.com".insteadOf "https://github.com"
+
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply peterbraden
 fi
 # }}}
