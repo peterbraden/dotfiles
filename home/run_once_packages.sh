f389a1cb35d95676465174164214a4b1ab5ba148
Peter Braden <peterbraden@peterbraden.co.uk>
2024-12-03 11:39:02 +0100

    wip

--- home/run_once_packages.sh
+++ home/run_once_packages.sh
@@ -56,10 +56,7 @@ fi
 # }}}
 
 # Linux {{{
-
-
-
-if sudo true; then
+if sudo true  4> /dev/null; then
   #sudo -v
   # Keep-alive: update existing `sudo` time stamp until the script has finished.
   #while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
