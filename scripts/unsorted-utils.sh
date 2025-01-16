a5da9897e0b8dcc8e855baeadf22f73f102f72b1
Peter Braden <peterbraden@peterbraden.co.uk>
2025-01-16 15:34:50 +0000

    WIP

---
+++ scripts/unsorted-utils.sh
@@ -0,0 +1,30 @@
+# Functions ----- 
+function psgrep() {
+EXP=`echo $1 | sed -e 's/^\(.\)/\[\1\]/'`
+ps aux | grep $EXP
+}
+
+function oldip(){
+ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+}
+
+extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
